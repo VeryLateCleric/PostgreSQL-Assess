67d3c9806876dbc60c072308638db97c9fbf488c
Kenneth <kennethyoung.2960@gmail.com>
2024-06-09 19:54:58 -0700

    Cleared up inserts and Artists tests are passing 4 of 11

--- src/artists-insert-multiple.sql
+++ src/artists-insert-multiple.sql
@@ -1,7 +1,6 @@
 INSERT INTO artists (artist_name, birthday, birthplace, is_alive)
-VALUES (
-    'Wassily Kandinsky',
-    '1866-12-16',
-    'Moscow, Russia',
-    FALSE
-    );
+VALUES
+('Leonardo da Vinci', '1452-04-15', 'Vinci, Republic of Florence', FALSE),
+('Claude Monet', '1840-11-14', 'Paris, France', FALSE),
+('Salvador Dalí', '1904-05-11', 'Figueres, Spain', FALSE),
+('Jackson Pollock', '1912-01-28', 'Cody, Wyoming, USA', FALSE);
